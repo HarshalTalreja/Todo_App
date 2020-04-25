612a5ff0f7aa1326b8d80573213361c2d963b26e
HarshalTalreja <talrejaharshal50@gmail.com>
2020-04-25 19:16:23 +0530

    Removed venv

--- todo_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Harshal Talreja\Documents\TODO_APP\todo_env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
